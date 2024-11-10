ccccaa587c031029a3720c2ec1aed503565f8d5a
programmingroup <luisgx14@gmail.com>
2024-11-10 00:45:23 -0500

    feat: :sparkles: Modifying index file

--- index.mjs
+++ index.mjs
@@ -7,9 +7,9 @@ const server = createServer((req, res) => {
   res.end('Hello Keyth! Welcome to my simple HTTP server in Node.js. This is an example of how to set up a basic web server that handles request on port 3000');
 });
 
-// starts a simple http server locally on port 3000
+
 server.listen(3000, '0.0.0.0', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
-// run with `node server.mjs`
+
