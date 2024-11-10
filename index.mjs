60b6399f458fa666422e53577c409f2c49be78e7
programmingroup <luisgx14@gmail.com>
2024-11-09 21:53:45 -0500

    feat: :sparkles: Creation http server with node.js

---
+++ index.mjs
@@ -0,0 +1,15 @@
+// server.mjs
+import { createServer } from 'node:http';
+
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Keyth! Welcome to my simple HTTP server in Node.js. This is an example of how to set up a basic web server that handles request on port 3000');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
